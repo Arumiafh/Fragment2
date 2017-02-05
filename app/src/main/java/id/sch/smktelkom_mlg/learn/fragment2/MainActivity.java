fee5693a3975d8dcc230d98e12c79341050d9628
arumiafh <arumiafh@gmail.com>
2017-02-05 11:32:58 -0800

    Fragment Change View 3 Fragment

--- app/src/main/java/id/sch/smktelkom_mlg/learn/fragment2/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/fragment2/MainActivity.java
@@ -7,7 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements View2Fragment.IListener {
     private int mNo;
     private TextView tvNumber;
 
@@ -46,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
                 .setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        if (mNo < 3) {
+                        if (mNo < 4) {
                             changePage(++mNo);
                         }
                     }
@@ -70,7 +71,18 @@ public class MainActivity extends AppCompatActivity {
                 fragment = View1Fragment.newInstance(
                         R.drawable.ic_wb_cloudy_black_24dp, "Awan");
                 break;
+            case 4:
+                fragment = new View2Fragment();
+                break;
+            case 5:
+                fragment = new View3Fragment();
+                break;
         }
         fm.beginTransaction().replace(R.id.container, fragment).commitNow();
+    }
+
+    @Override
+    public void toPromoPage() {
+        changeFragment(5);
     }
 }
